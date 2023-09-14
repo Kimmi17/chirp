e4f5baee1afabd59feca5a936c8006a68859fdd3
Kimmi17 <dangthikimthuybl@gmail.com>
2023-09-14 04:37:39 +0300

    Next image tag, styleup

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,13 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "img.clerk.com",
+      },
+    ],
+  },
 
   /**
    * If you are using `appDir` then you must comment the below `i18n` config out.
